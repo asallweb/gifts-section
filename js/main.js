26c339e47893c1c9a19a87844debb7148244c049
Thief8925 <asallweb@gmail.com>
2022-05-18 18:53:34 +0200

    v1.0

--- js/main.js
+++ js/main.js
@@ -1,25 +1,8 @@
-window.addEventListener('load', function(){
-	setTimeout(function () {
-		/* Animate.css initialize */
-		$('.hasAnimation').each(function () {
-			var animateName = $(this).data('animate');
-			$(this).addClass("hidden").viewportChecker({
-				classToRemove: 'hidden',
-				classToAdd: 'visible animate__animated ' + animateName,
-				offset: 0
-			});
-		});	
-	}, 200);
-});
 $(document).ready(function () {
-	/* Phone validation */
-	$(".wpcf7-validates-as-tel").mask("+7 (999) 999-9999");
-
-	/* Popup initialize */
-	popupInitialize();
-
-
-});
-$(window).resize(function () {
-
+	$('.gift__popup .toggle-gift__section').click(function(){
+		$(this).parent().find('.gift__section').slideToggle();
+	});
+	$('.gift__popup .gift__section .head button').click(function(){
+		$(this).parent().parent().slideToggle();
+	});
 });
